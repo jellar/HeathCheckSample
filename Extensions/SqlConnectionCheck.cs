ecb40d7ffa66aa85af143447cc83178ec7519192
Rajesh Jella <jella.rajesh@gmail.com>
2020-10-18 17:37:00 +0100

    Initial commit

---
+++ Extensions/SqlConnectionCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckSample.Extensions
+{
+    public class SqlConnectionCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
