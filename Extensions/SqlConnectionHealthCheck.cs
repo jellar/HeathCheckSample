73c80212e55db3f1b102840627df5717afea72fc
Rajesh Jella <jella.rajesh@gmail.com>
2020-10-19 16:04:53 +0100

    Health check UI added

---
+++ Extensions/SqlConnectionHealthCheck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Net.Mime;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.VisualBasic;
+
+namespace HealthCheckSample.Extensions
+{
+    public class SqlConnectionHealthCheck : IHealthCheck
+    {
+        private static readonly string DefaultTestQuery = "Select 1";
+        public string TestQuery { get; }
+        public string ConnectionString { get; }
+
+        public SqlConnectionHealthCheck(string connectionString) : this(connectionString, DefaultTestQuery)
+        {
+            
+        }
+
+        public SqlConnectionHealthCheck(string connectionString, string testQuery)
+        {
+            ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+            TestQuery = testQuery;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync(cancellationToken);
+                    if (TestQuery != null)
+                    {
+                        var command = connection.CreateCommand();
+                        command.CommandText = TestQuery;
+
+                        await command.ExecuteNonQueryAsync(cancellationToken);
+                    }
+
+                }
+                catch (DbException ex)
+                {
+                    return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
+                }
+
+                return HealthCheckResult.Healthy();
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,22 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HealthChecks.System;
+using HealthChecks.UI.Client;
+using HealthChecks.UI.Configuration;
+using HealthCheckSample.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 
 namespace HealthCheckSample
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHealthChecks();
+            services.AddHealthChecks()
+                .AddDiskStorageHealthCheck(delegate(DiskStorageOptions options)
+                {
+                    options.AddDrive(@"C:\", minimumFreeMegabytes: 5000);
+                }, "My Drive", HealthStatus.Degraded)
+                //.AddSqlServer(Configuration["ConnectionStrings:DefaultConnection"]);
+                .AddCheck("my database",
+                    new SqlConnectionHealthCheck(Configuration["ConnectionStrings:DefaultConnection"]))
+                ;
+
+            services.AddHealthChecksUI();
         }
+
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -27,7 +51,16 @@ namespace HealthCheckSample
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHealthChecks("/hc");
+            app.UseHealthChecks("/hc", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            app.UseHealthChecksUI(delegate(Options options)
+            {
+                options.UIPath = "/hc-ui";
+            });
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
